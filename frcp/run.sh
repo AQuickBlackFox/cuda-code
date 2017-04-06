b0d5fad9039eaf0cdd7e459fa80d8555534f331f
Aditya Atluri <Aditya.Atluri@amd.com>
2017-04-06 13:05:49 -0500

    added frcp bm

---
+++ frcp/run.sh
@@ -0,0 +1,308 @@
+#!/bin/bash
+
+touch file.txt
+
+
+for i in {1..5}
+do
+    ./a.out 0 128 28 
+done
+./a.out 0 128 28 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 256 28 
+done
+./a.out 0 256 28 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 512 28 
+done
+./a.out 0 512 28 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 1024 28 
+done
+./a.out 0 1024 28 >> file.txt
+
+
+for i in {1..5}
+do
+    ./a.out 0 64 56
+done
+./a.out 0 64 56 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 128 56
+done
+./a.out 0 128 56 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 256 56
+done
+./a.out 0 256 56 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 512 56 
+done
+./a.out 0 512 56 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 1024 56 
+done
+./a.out 0 1024 56 >> file.txt
+
+
+
+for i in {1..5}
+do
+    ./a.out 0 32 112
+done
+./a.out 0 32 112 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 64 112
+done
+./a.out 0 64 112 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 128 112
+done
+./a.out 0 128 112 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 256 112
+done
+./a.out 0 256 112 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 512 112 
+done
+./a.out 0 512 112 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 1024 112
+done
+./a.out 0 1024 112 >> file.txt
+
+
+
+for i in {1..5}
+do
+    ./a.out 0 16 224
+done
+./a.out 0 16 224 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 32 224
+done
+./a.out 0 32 224 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 64 224
+done
+./a.out 0 64 224 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 128 224
+done
+./a.out 0 128 224 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 256 224
+done
+./a.out 0 256 224 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 512 224
+done
+./a.out 0 512 224 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 1024 224
+done
+./a.out 0 1024 224 >> file.txt
+
+
+for i in {1..5}
+do
+    ./a.out 0 8 448
+done
+./a.out 0 8 448 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 16 448
+done
+./a.out 0 16 448 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 32 448
+done
+./a.out 0 32 448 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 64 448
+done
+./a.out 0 64 448 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 128 448
+done
+./a.out 0 128 448 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 256 448
+done
+./a.out 0 256 448 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 512 448
+done
+./a.out 0 512 448 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 1024 448
+done
+./a.out 0 1024 448 >> file.txt
+
+
+for i in {1..5}
+do
+    ./a.out 0 4 896
+done
+./a.out 0 4 896 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 8 896
+done
+./a.out 0 8 896 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 16 896
+done
+./a.out 0 16 896 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 32 896
+done
+./a.out 0 32 896 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 64 896
+done
+./a.out 0 64 896 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 128 896
+done
+./a.out 0 128 896 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 256 896
+done
+./a.out 0 256 896 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 512 896
+done
+./a.out 0 512 896 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 1024 896
+done
+./a.out 0 1024 896 >> file.txt
+
+
+for i in {1..5}
+do
+    ./a.out 0 2 1792
+done
+./a.out 0 2 1792 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 4 1792
+done
+./a.out 0 4 1792 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 8 1792
+done
+./a.out 0 8 1792 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 16 1792
+done
+./a.out 0 16 1792 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 32 1792
+done
+./a.out 0 32 1792 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 64 1792
+done
+./a.out 0 64 1792 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 128 1792
+done
+./a.out 0 128 1792 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 256 1792
+done
+./a.out 0 256 1792 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 512 1792
+done
+./a.out 0 512 1792 >> file.txt
+
+for i in {1..5}
+do
+    ./a.out 0 1024 1792
+done
+./a.out 0 1024 1792 >> file.txt
+
+
